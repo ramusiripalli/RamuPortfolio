877494958e0eb8bc7de717773be29e66756a1e36
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-21 12:49:47 +0530

    updated resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar(){
             <h6 className="text-3xl animate-spin">MERN</h6>
         </div>
         <div className="m-8 flex items-center justify-center gap-6 text-4xl">
-        <a href="https://drive.google.com/file/d/18gC7Riiwxl4Z58skvtSBPLBct7Yn9PQQ/view?usp=sharing" target="_blank" className="animate-bounce w-6 h-6"><abbr title="Resume"><FaFileDownload /></abbr></a>
+        <a href="https://drive.google.com/file/d/1C1Tc7XWsC4k_8knMHJrNp5MdgncGFyyj/view?usp=sharing" target="_blank" className="animate-bounce w-6 h-6"><abbr title="Resume"><FaFileDownload /></abbr></a>
             <a href="https://www.linkedin.com/in/ramu-siripalli-3711bb119/" target="_blank" className="animate-bounce w-6 h-6"><abbr title="Linked In"><FaLinkedin /></abbr></a>
             <a href="https://github.com/ramusiripalli" target="_blank" className="animate-bounce w-6 h-6"><abbr title="GitHub"><FaGithub /></abbr></a>
             <a href="https://leetcode.com/u/Ramu2772/" target="_blank" className="animate-bounce w-6 h-6"><abbr title="Leetcode"><TbBrandLeetcode /></abbr></a>
